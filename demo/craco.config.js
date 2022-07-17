e51421d43ecab4a83799606c59af47ab32ff9d79
xiaosong <kunkkaco@gmail.com>
2022-07-17 16:50:14 +0800

    chore: 使用 dayjs 替换 moment.js 🛠️

--- demo/craco.config.js
+++ demo/craco.config.js
@@ -1,6 +1,26 @@
 const CracoLessPlugin = require("craco-less");
+const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
+// 配置文件说明文档：https://github.com/dilanx/craco/blob/master/packages/craco/README.md
 module.exports = {
+  webpack: {
+    // alias: {},
+    plugins: {
+      /* Specify if plugin should be appended or prepended */
+      /* An array of plugins */
+      add: [
+        new AntdDayjsWebpackPlugin(),
+        // new BundleAnalyzerPlugin({ analyzerMode: "server" }),
+      ],
+      /* An array of plugin constructor's names (i.e. "StyleLintPlugin", "ESLintWebpackPlugin" ) */
+      // remove: [],
+    },
+    /* Any webpack configuration options: https://webpack.js.org/configuration */
+    // configure: (webpackConfig, { env, paths }) => {
+    //   return webpackConfig;
+    // },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
